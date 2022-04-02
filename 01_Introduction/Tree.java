9c8099d328bdfba02f77981c4c9f394db30e3f92
benedictusdps <benedictusdps@gmail.com>
2022-04-02 16:40:43 +0700

    Add files from upload

---
+++ 01_Introduction/Tree.java
@@ -0,0 +1,18 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args){
+    /* 
+    This program will introduce ourselves 
+    and print a tree to the screen
+    */
+    System.out.println("Hey there, I'm Beno and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("*****");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+  }
+}
