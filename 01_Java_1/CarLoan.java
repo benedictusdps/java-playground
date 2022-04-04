90f2c53f52b73cf1c8cf23615cbc98199e88b5df
benedictusdps <benedictusdps@gmail.com>
2022-04-04 07:36:29 +0700

    Add files from upload

---
+++ 01_Java_1/CarLoan.java
@@ -0,0 +1,27 @@
+public class CarLoan {
+  // This program calculates required monthly payment for a car loan
+	public static void main(String[] args) {
+        // amount of car loan
+        int carLoan = 10000;
+        // length of car loan
+        int loanLength = 3;
+        // interest rate of car loan
+        int interestRate = 5;
+        // down payment provided by user
+        int downPayment = 2000;
+
+        // loan requirements
+        if (loanLength <= 0 || interestRate <= 0) {
+        System.out.println("Error! You must take out a valid car loan.");
+        } else if (downPayment > carLoan) {
+        System.out.println("The car can be paid in full.");
+        } else {
+        int remainingBalance = carLoan - downPayment;
+        int months = loanLength * 12;
+        int monthlyBalance = remainingBalance / months;
+        int interest = monthlyBalance * interestRate / 100;
+        int monthlyPayment = monthlyBalance + interest;
+        System.out.println(monthlyPayment);
+        }
+	}
+}
